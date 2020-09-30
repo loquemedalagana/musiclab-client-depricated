4f14eae95b5088ede1cfaf81ac65d1705a3ab2d5
loquemedalagana <nadamasquetu012@gmail.com>
2020-09-30 22:58:50 +0900

    나중에 페이지 만들기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import store from './app/store';
 import { Provider } from 'react-redux';
+import { Router, Route, Switch } from "react-router-dom";
+import Routes from './routes/routes';
 
 function App() {
+  //load and store user state
+
   return (
     <Provider store = {store}>
       <h1>hello world</h1>
+      <Router>
+        <Switch>
+          <Route exact path = '/' component = {null} />
+          <Route component = {Routes} /> 
+        </Switch>
+      </Router>
     </Provider>
   );
 }
---
+++ src/routes/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Switch, Route } from "react-router-dom";
+
+export default props => (
+    <>
+        <Switch>
+            <Route exact path = '/login' component = {null} />
+            
+        </Switch>
+    </>
+);
