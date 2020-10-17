4d75d202c04c15a32c8b01759bd83fa1d8214b1a
loquemedalagana <nadamasquetu012@gmail.com>
2020-10-17 15:52:58 +0900

    add username in navbar

--- src/components/Header/HeaderLinks.js
+++ src/components/Header/HeaderLinks.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 // react components for routing our app without refresh
 import { withRouter } from "react-router-dom";
+import { authSelector } from '../../app/store/auth';
 
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
@@ -30,18 +31,18 @@ import Button from "../CustomButtons/Button.js";
 import styles from "../../assets/jss/material-kit-react/components/headerLinksStyle.js";
 const useStyles = makeStyles(styles);
 
-const notificationCnt = 0, chatCnt = 7;
-
 const HeaderLinks = (props) => {
   const classes = useStyles();
   const {
     setMobileOpen,
     logoutUser,
+    username,
     isAuth,
     isAdmin,
     isMember,
     history,
     userId,
+    notifications,
   } = props;
 
   if(!isAuth) return <Redirect to = '/' />;
@@ -97,8 +98,8 @@ const HeaderLinks = (props) => {
       <ListItem className={classes.listItem}>
         <CustomDropdown
           noLiPadding
-          buttonText="mypage"
-          badgeContent={notificationCnt+chatCnt}
+          buttonText={username}
+          badgeContent={notifications.general + notifications.chat}
           buttonProps={{
             className: classes.navLink,
             color: "transparent"
@@ -114,7 +115,7 @@ const HeaderLinks = (props) => {
             </Button>,
             <Button color = "transparent" onClick={()=>console.log('notifications')} className={classes.dropdownLink}>
               <Badge 
-                badgeContent={notificationCnt} 
+                badgeContent={notifications.general} 
                 variant='dot' 
                 color="secondary"
                 anchorOrigin={{
@@ -128,7 +129,7 @@ const HeaderLinks = (props) => {
             </Button>,
             <Button color = "transparent" onClick={()=>console.log('private msg')} className={classes.dropdownLink}>
               <Badge 
-                badgeContent={chatCnt} 
+                badgeContent={notifications.chat} 
                 variant='dot' 
                 color="secondary"
                 anchorOrigin={{
@@ -175,17 +176,24 @@ HeaderLinks.propTypes = {
   setMobileOpen: PropTypes.func,
   history: PropTypes.object,
   logoutUser: PropTypes.func,
+  username: PropTypes.string,
   userId: PropTypes.string,
   isAuth: PropTypes.bool,
   isMember: PropTypes.bool,
   isAdmin: PropTypes.bool,
+  notifications: PropTypes.object,
 }
 
 const mapStateToProps = (state) => ({
   isAuth: state.auth.auth,
+  username: state.auth.userData ? state.auth.userData.displayName : undefined,
   userId: state.auth.userData ? state.auth.userData._id : undefined,
   isMember: state.auth.auth === true ? (state.auth.userData && state.auth.userData.points >= 0) : false,
   isAdmin: state.auth.auth === true ? (state.auth.userData && state.auth.userData.isadmin) : false,
+  notifications: {
+    general: state.auth.userData ? state.auth.userData.notifications.length : 0,
+    chat: 4,
+  }
 })
 
 
