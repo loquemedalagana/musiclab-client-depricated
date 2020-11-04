f90a5fef4d734a69c451ba236024e9aa6d9369e3
loquemedalagana <nadamasquetu012@gmail.com>
2020-11-04 19:30:51 +0900

    토큰 검증 여부 체크 성공!

--- src/routes/HigherOrderComponets/TokenRoute.js
+++ src/routes/HigherOrderComponets/TokenRoute.js
@@ -7,12 +7,10 @@ import fetcher from '../../app/fetcher';
 import useSWR from 'swr';
 
 import Loading from '../../components/Loading/LinearLoading';
-//import Notfound from '../Pages/NotFound';
 //levelup, passwordreset page
 
 const TokenRoute = ({
     component: Component,
-    user,
     isChanged,
     location,
     ...rest
@@ -22,34 +20,34 @@ const TokenRoute = ({
         ignoreQueryPrefix: true
     });  
     const {token, expiredtime} = query;
-    console.log(query, token, Date.now(), expiredtime);
 
     const ENDPOINT = `/api/user/checktoken/?token=${token}&expiredtime=${expiredtime}`;
     const isExpired = Date.now() > expiredtime;
     const {data, error} = useSWR(isExpired ? null : ENDPOINT, isExpired ? null : fetcher);
 
-    console.log(data, error);   
+    //console.log(data, error);   
     console.log(isExpired ? "expired!" : null);
-    //const LoadingToken = !data && !error;
     
+    const LoadingToken = !data && !error && !isExpired;
 
     return ( <Route
         {...rest}
         render={props =>
-            user.loading ? (
+            LoadingToken ? (
                 <Loading />
-            ) : <Component {...props} />
+            ) : ( error || isExpired ? <Redirect to = '/notfound' />
+                : <Component {...props} />
+            )
         }
     />
 )};
 
 TokenRoute.propTypes = {
-    user: PropTypes.object.isRequired,
     isChanged: PropTypes.bool,
 };
 
 const mapStateToProps = (state) => ({
-    user: state.auth,
+
     isChanged: state.userValidation.changed
 });
 
