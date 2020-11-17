69f32128cce8ae44605855f243a8ee1d1643cf59
loquemedalagana <nadamasquetu012@gmail.com>
2020-11-17 20:25:13 +0900

    add carousel

--- src/views/Landing/Sections/VideoCarouselSection.js
+++ src/views/Landing/Sections/VideoCarouselSection.js
@@ -4,17 +4,30 @@ import { connect } from 'react-redux';
 import fetcher from '../../../app/fetcher';
 import useSWR from 'swr';
 import { makeStyles } from "@material-ui/core/styles";
+import Slider from "react-slick";
 
 import {
     GridContainer,
     GridItem,
     CircularLoading,
+    Card,
 } from '../../../components/components';
+
+//import {MusicNoteRounded} from '@material-ui/icons';
 
 import styles from '../../../assets/jss/material-kit-react/components/carouselStyle';
 import { getPlayListURL, JeonInhyukBandPlayListEndPoint } from '../../../app/videoFetchEndpoints';
 
 const useStyles = makeStyles(styles);
+
+//print title
+const printTitle = type => (<GridItem>
+    <h2>{type}</h2>
+</GridItem>)
+
+const NotAvailable = () => (<GridItem>
+    <h3>준비중입니다...</h3>
+</GridItem>)
 
 const getPlayListId = type => {
     switch(type){
@@ -27,35 +40,79 @@ const getPlayListId = type => {
     }
 }
 
-//print title
-const printTitle = type => (<GridItem>
-    <h2>{type}</h2>
-</GridItem>)
+//carosel element component
+const CaroselElement = props => {
+    const {
+        data,
+    } = props;
 
-const NotAvailable = () => (<GridItem>
-    <h3>준비중입니다...</h3>
-</GridItem>)
+    const {
+        title,
+        thumbnail
+    } = data;
+
+    //글자가 너무 길면 자르기
+
+    return (
+        <div>
+            <img src={thumbnail} alt={title} className="slick-image" />
+        </div>
+    )
+}
+
 
 export const VideoCarouselSection = props => {
     const classes = useStyles();
-
     const {
         type, //jihbandoifficialchannel, musicsseolprisechannel, 
     } = props;
 
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+
+        responsive: [
+            {
+                breakpoint: 1260,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    
+                }
+            }
+        ]
+    }
+
     const playListId = getPlayListId(type);
-    const ENDPOINT = getPlayListURL(playListId, 5);
+    const ENDPOINT = getPlayListURL(playListId, 6);
 
     //get api
     const {data, error} = useSWR(playListId ? ENDPOINT : null, fetcher);
     //console.log(data, error);
 
     //get result data
-    const resultData = data ? data.items.map(item => {
-        //console.log(item.snippet);
-        //console.log(item.contentDetails);
-        //console.log(item.snippet.thumbnails);
-        
+    const resultData = data ? data.items.map(item => {        
         return ({
             title: item.snippet.title,
             channelTitle: item.snippet.channelTitle,
@@ -88,6 +145,15 @@ export const VideoCarouselSection = props => {
                 <GridContainer>
                     {printTitle(type)}
                     
+                    <GridItem>
+                        <Card carousel>
+                            <Slider {...settings}>
+                            {resultData.map((item, key) => 
+                                <CaroselElement key={key} data={item} />
+                            )}
+                            </Slider>
+                        </Card>
+                    </GridItem>
 
                 </GridContainer>
             </div>
