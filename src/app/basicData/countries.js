af143a0c5a0f1f30154c14785a539759aaa9c72d
loquemedalagana <nadamasquetu012@gmail.com>
2020-12-02 21:24:09 +0900

    update data and readme

--- src/app/basicData/countries.js
+++ src/app/basicData/countries.js
@@ -1,4 +1,4 @@
-export const countries = [
+export default [
   { code: 'AD', label: 'Andorra', phone: '376' },
   { code: 'AE', label: 'United Arab Emirates', phone: '971' },
   { code: 'AF', label: 'Afghanistan', phone: '93' },
--- src/app/videoData/SampleData/SampleData.js
+++ src/app/videoData/SampleData/SampleData.js
@@ -1,118 +1,127 @@
 export const SampleVideoList = [
-    {
+  {
         channelTitle: "JTBC Entertainment",
         publishedAt: "2016-01-05T14:38:37Z",
         thumbnail: "https://i.ytimg.com/vi/pTer_gdmQfQ/hqdefault.jpg",
         title: "[희열팀] 슈가송 야다 ,이미 슬픈 사랑,♪ 슈가맨 12회",
         videoId: "pTer_gdmQfQ",
-    },
-    {
+  },
+  {
         channelTitle: "근황올림픽",
         publishedAt: "2020-09-28T11:00:00Z",
         thumbnail: "https://i.ytimg.com/vi/auqUW6nGZQ0/hqdefault.jpg",
         title: "[야다를 만나다] 레전드 보컬 전인혁 훈남 근황 .. ,이미 슬픈사랑, 한 소절 불러주셨습니다",
         videoId: "auqUW6nGZQ0"
-    },
-    {
+  },
+  {
         channelTitle: "TBE",
         publishedAt: "2017-08-13T06:43:33Z",
         thumbnail: "https://i.ytimg.com/vi/t1iMkAcPSoA/hqdefault.jpg",
         title: "전인혁 인하대 후문 버스킹 영상 - 팬과 함께 부르는 ,진혼,",
         videoId: "t1iMkAcPSoA"
-    },
-    {
+  },
+  {
         channelTitle: "야다덕후",
         publishedAt: "2020-07-16T16:08:33Z",
         thumbnail: "https://i.ytimg.com/vi/lv4gaXD8Vwk/hqdefault.jpg",
         title: "전인혁(야다) 고음, 라이브 모음",
         videoId: "lv4gaXD8Vwk",
-    },
-    {
+  },
+  {
         channelTitle: "TBE",
         publishedAt: "2017-08-13T06:36:27Z",
         thumbnail: "https://i.ytimg.com/vi/mtn7POTYit0/hqdefault.jpg",
         title: "전인혁 인하대 후문 버스킹 영상 - ,이미 슬픈 사랑,",
         videoId: "mtn7POTYit0",
-    },
-    {
+  },
+  {
         channelTitle: "Shane Lee",
         publishedAt: "2014-05-07T07:47:39Z",
         thumbnail: "https://i.ytimg.com/vi/SaJRpu9ASxs/hqdefault.jpg",
         title: "야다 전인혁 샤우팅 모음",
         videoId: "SaJRpu9ASxs"
-    },
-    {
+  },
+  {
         channelTitle: "이재림의 카잼TV_Studio CAR JAM",
         publishedAt: "2020-11-21T04:10:55Z",
         thumbnail: "https://i.ytimg.com/vi/UMsZti6fpMA/hqdefault.jpg",
         title: "볼보 S60 B5 인스크립션 2021 B&amp;W오디오 테스트 with 전인혁(야다 보컬)",
         videoId: "UMsZti6fpMA"
-    },
-    {
+  },
+  {
         channelTitle: "YADAInhyuk",
         publishedAt: "2011-01-06T21:42:12Z",
         thumbnail: "https://i.ytimg.com/vi/uxbxUP2s26g/hqdefault.jpg",
         title: "YADA - Love to Sorrow (Live) - 사랑이 슬픔에게 - 야다",
         videoId: "uxbxUP2s26g"
-    },
-    {
+  },
+  {
         channelTitle: "wjdthdus89",
         publishedAt: "2015-01-24T12:09:24Z",
         thumbnail: "https://i.ytimg.com/vi/2e1TfSjp07s/hqdefault.jpg",
         title: "전인혁 - ㅂㅎㅇㅁㄱ",
         videoId: "2e1TfSjp07s",
-    },
-    {
+  },
+  {
         channelTitle: "버블티",
         publishedAt: "2020-06-09T17:00:51Z",
         thumbnail: "https://i.ytimg.com/vi/CA0GICPBmf4/hqdefault.jpg",
         title: "[Live] 야다(Yada) 슬픈 다짐 [뮤직뱅크 2003년 4월 2주]",
         videoId: "CA0GICPBmf4"
-    },
-    {
+  },
+  {
         channelTitle: "JTBC Entertainment",
         publishedAt: "2015-08-18T13:50:25Z",
         thumbnail: "https://i.ytimg.com/vi/r1KRX4Rhjx4/hqdefault.jpg",
         title: "야다 전인혁 ,그대 떠나가도♪, 명품 보이스 끝까지 간다 39회",
         videoId: "r1KRX4Rhjx4"
-    },
-    {
+  },
+  {
         channelTitle: "현진영테레비",
         publishedAt: "2019-12-21T11:23:06Z",
         thumbnail: "https://i.ytimg.com/vi/Q5H8Qui2Hes/hqdefault.jpg",
         title: "야다-전인혁 미스터투-이민규 무반주 쌩목 라이브 매불쇼(현진영데이)",
         videoId: "Q5H8Qui2Hes"
-    },
-    {
+  },
+  {
         channelTitle: "suntube김선희",
         publishedAt: "2019-12-22T14:25:19Z",
         thumbnail: "https://i.ytimg.com/vi/boymV8E7EKo/hqdefault.jpg",
         title: ",진혼, 게스트 전인혁 플라워 2019년 20주년 콘서트",
         videoId: "boymV8E7EKo"
-    }
+  }, 
+  {
+        channelTitle : "NECORD MUSIC",
+        publishedAt : "2017-12-05T08:08:52Z",
+        thumbnail: "https://i.ytimg.com/vi/vzX3g7XcnOc/sddefault.jpg",
+        description : "《2017 DnF Live Concert》\n2017. 10. 21 PM 5 - 7\n@Blue Square Hall\n\n☞ Live Session\nVocal 전인혁\nGuitar 신용준, 김현규\nBass 신재호\nDrum 김대호\nKeyboard 김지인, 백지훈",
+        title: "[2017 DnF LIVE CONCERT] 전인혁 - Man's Road",
+        channelId: "UC7gy0ee1jeNO11HievGQJzA",
+        videoId: "vzX3g7XcnOc"
+  }
 ]
 
 
 export const SampleTagList = [
-    {
+  {
         tagName: 'vocal',
-    },
-    {
+  },
+  {
         tagName: 'drum',
-    },
-    {
+  },
+  {
         tagName: 'guitar',
-    },
-    {
+  },
+  {
         tagName: 'bass',
-    },
-    {
+  },
+  {
         tagName: 'piano',
-    },
-    {
+  },
+  {
         tagName: 'yada',
-    },
-    {
+  },
+  {
         tagName: 'jeoninhyukband',
-    },
+  },
 ]
