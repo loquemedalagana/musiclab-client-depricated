e09d0de87742ded6ecf5f739f07617da86d9b35a
loquemedalagana <nadamasquetu012@gmail.com>
2020-12-05 22:41:17 +0900

    refactored video iframe

--- src/components/VideoIframe/VideoIframe.js
+++ src/components/VideoIframe/VideoIframe.js
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-const VideoIframe = props => {
-  const {
-    videoId,
-    width,
-    height,
-  } = props;
+const VideoIframe = (props) => {
+  const { videoId, width, height } = props;
 
-  const videoURL = `https://www.youtube.com/embed/${videoId}` 
+  const videoURL = `https://www.youtube.com/embed/${videoId}`;
 
   return (
-    <iframe 
-      width={width} 
-      height={height} 
+    <iframe
+      width={width}
+      height={height}
       src={videoURL}
-      frameBorder="0" 
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
-    >
-    </iframe>
-  )
-}
+    ></iframe>
+  );
+};
 
-export default VideoIframe
+VideoIframe.propTypes = {
+  videoId: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string,
+};
+
+export default VideoIframe;
